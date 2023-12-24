027a8600d8be77ca5fd9063c2b1ea0a0a4b9a2dd
GaLaiLapTrinh <galailaptrinh@gmail.com>
2023-12-24 20:23:53 +0700

    3.update 5.2 quy tắc đặt tên biến js

---
+++ 5.1-giai-baitap01/main.js
@@ -0,0 +1,8 @@
+// khai báo biến
+// let userName;
+
+// hiển thị hộp nhập liệu
+let userName = prompt("Mời cụ nhập tên: ");
+console.log(userName);
+console.log("Tên của bạn là: " + userName);
+console.log("Chào mừng " + userName + " đến với trang tuhoc.cc");
---
+++ 5.2-quytac-datten-bien/main.js
@@ -0,0 +1,15 @@
+// tên biến hợp lệ ( bắt đầu chữ cái, _, $)
+let name;
+let Name;
+let _name;
+let $name;
+// các trường hợp không hợp lệ
+// let 1name;
+// let @name;
+
+// let var;
+// let const;
+
+//camelCase
+let doSoi;
+let doSoiCuaNuoc;
