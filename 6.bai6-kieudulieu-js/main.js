1e3a4ce4cb22eee07a5dd1d42bc87462d4cabd90
GaLaiLapTrinh <galailaptrinh@gmail.com>
2023-12-25 21:26:00 +0700

    4.update code bài 6-data-type

---
+++ 6.bai6-kieudulieu-js/main.js
@@ -0,0 +1,26 @@
+//kiểu number
+let soNguyen = 10;
+let soThuc = 3.14;
+
+// kiểu boolean
+let check = false;
+
+// kiểu undefined
+let diemToan;
+
+//kiểu null
+let connect = null;
+
+// xuất giá trị của biến
+console.log(soNguyen);
+console.log(soThuc);
+console.log(typeof soNguyen);
+console.log(typeof soThuc);
+
+console.log(typeof check);
+console.log(typeof diemToan);
+console.log(typeof connect);
+
+// xuất số nguyên an toàn tối đa/ tối thiểu
+console.log("Số nguyên an toàn tối đa: " + Number.MAX_SAFE_INTEGER);
+console.log("Số nguyên an toàn tối thiểu: ", Number.MIN_SAFE_INTEGER);
