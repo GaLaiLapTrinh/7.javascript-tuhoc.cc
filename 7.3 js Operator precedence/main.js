28ff77345a132d49f1176b2b4798126e02feb77a
GaLaiLapTrinh <galailaptrinh@gmail.com>
2024-01-05 09:35:47 +0700

    8. update lý thuyết bài 7.3

---
+++ 7.3 js Operator precedence/main.js
@@ -0,0 +1,11 @@
+// Ưu tiên ()
+let diemToan = 9.5;
+let diemVan = 8.75;
+
+let dtb = (diemToan + diemVan) / 2;
+console.log(dtb);
+
+// right to left
+let x = (y = 25);
+// left to right
+let c = 25 - 7 + 8 - 1;
